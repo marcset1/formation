340782c210c2304d463749498796903f7a625c92
marcset1 <dvisalla2@gmail.com>
2025-03-17 05:11:05 +0100

    backend: correction erreur - result undefined at insert product

--- reactecom1/frontend/src/pages/Cart.jsx
+++ reactecom1/frontend/src/pages/Cart.jsx
@@ -18,7 +18,7 @@ const Cart = () => {
 
     if (name && phone && city) {
       try {
-        const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/orders/validate`, {
+        const response = await axios.post(`https://ventelivre.onrender.com/api/orders/validate`, {
           nom: name,
           prenom: '', // Si tu as un champ prénom, utilise-le ici
           email: email,
